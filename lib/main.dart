bb395fc215a59cf8bb646c1572ecb3e2a8d06fe9
seki019 <seeeeeki19@gmail.com>
2025-08-18 23:01:56 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'doctor.dart';
+import 'teacher.dart';
+
 void main() {
   Doctor doc = Doctor("Smith", "Cardiology");
+  Teacher teacher = Teacher("Johnson", "Mathematics");
  
   doc.introduce();
+  teacher.introduce();
 }
