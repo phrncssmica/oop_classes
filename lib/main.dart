5b4c52026a992210dbd736075e63c103c523d141
cassandranoquera997-boop <cassandranoquera997@gmail.com>
2025-08-19 15:12:42 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'doctor.dart';
 import 'teacher.dart';
+import 'engineer.dart';
 void main() {
   Doctor doc = Doctor("Smith", "Cardiology");
   Teacher teacher = Teacher("Johnson", "Mathematics");
- 
+  Engineer engineer = Engineer("Alice", "Software Development");
   doc.introduce();
   teacher.introduce();
+  engineer.introduce();
 }
