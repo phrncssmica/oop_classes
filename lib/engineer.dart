cdcbc22f61ba953273d97c5d1ebe0407cd39df0a
cassandranoquera997-boop <cassandranoquera997@gmail.com>
2025-08-19 15:10:16 +0800

    Create engineer.dart

---
+++ lib/engineer.dart
@@ -0,0 +1,11 @@
+// Engineer class
+class Engineer {
+  String name;
+  String field;
+
+  Engineer(this.name, this.field);
+
+  void introduce() {
+    print("Hey, I am $name and I work as an engineer in $field.");
+  }
+}
